36f5df1abfe4ec81d8271724bf1ee5a1447eee63
5talyaqw <talyamalka2000@gmail.com>
2026-08-20 11:32:03 +0300

    dvd done

---
+++ Story_screen/DVD_player.py
@@ -0,0 +1,23 @@
+import os
+from google import genai
+import pygame
+from gtts import gTTS
+
+dvd_img = pygame.image.load("dvd_disk.png")
+dvd_img = pygame.transform.scale(dvd_img, (20, 30))
+
+client = genai.Client()
+model_ai = "gemini-3.6-flash"
+
+language = 'iw'
+def get_summery(sub):
+    res = client.interactions.create(
+        model=model_ai,
+        input=f"Write me a summery for the subject {sub}, in hebrew",
+    )
+    text_to_speech(res.output_text)
+
+def text_to_speech(text):
+    obj = gTTS(text=text, lang=language, slow=False)
+    obj.save(f"summery.mp3")
+    os.system(f"start summery.mp3")
